f2aa982646bf02a5d92060e4f4460b607d399cfe
BiDaPao <bdapao123@163.com>
2022-03-17 11:14:37 +0800

    测试

---
+++ src/test/java/com/example/springbootworkspace/SpringbootWorkspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootworkspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWorkspaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
